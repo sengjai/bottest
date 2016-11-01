9ce36a972fa254809ffb969c63f891ba4cc16dce
Abdullah Al-Thawr <aalthawr@gmail.com>
2016-11-01 16:43:53 +0800

    heroku routes2

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -3,7 +3,7 @@ def webhook
 		if params['hub.verify_token'] == 'mytoken'
 			render text: params['hub.challenge'] and return
 		else
-			render text: 'error' and return
+			render text: params['hub.verify_token'] and return
 		end
 	end
 end
