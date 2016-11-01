e56c091e5b326c5a5a52b2a614567d9be41e1b5f
Abdullah Al-Thawr <aalthawr@gmail.com>
2016-11-01 17:03:58 +0800

    heroku routes222

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,9 +1,15 @@
 class BotController < ApplicationController
-def webhook
+	def webhook	
 		if params['hub.verify_token'] == 'mytoken'
 			render text: params['hub.challenge'] and return
 		else
 			render text: 'error' and return
 		end
 	end
+
+	def receive_message
+
+		render text: 'hello' and return
+	end	
 end
+
